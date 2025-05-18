e90394c643578e39bbcfc1296bf02b1cba424557
cantilloIsabella <cantilloisabella@uninorte.edu.co>
2025-05-18 17:35:47 -0500

    Create passanger (validation)

--- src/core/controllers/PlaneController.java
+++ src/core/controllers/PlaneController.java
@@ -15,18 +15,22 @@ public class PlaneController {
 
     public PlaneController() {
     }
-    
-    public Response createPlane(String id, String brand, String model, String maxCapacity, String Airline){
+
+    public Response createPlane(String id, String brand, String model, String maxCapacity, String Airline) {
         try {
-            
+
             //Verify lenght of id
-            if(id.length()!=7){
-                  return new Response("The length of the Id must be 7 characters", Status.BAD_REQUEST);
+            if (id.length() != 7) {
+                return new Response("The length of the Id must be 7 characters", Status.BAD_REQUEST);
             }
-            
+
             //Verify characters in the id
         } catch (Exception e) {
             return new Response("Internal Server Error", Status.INTERNAL_SERVER_ERROR);
         }
+        
+        
+        return new Response("Internal Server Error", Status.INTERNAL_SERVER_ERROR);
+
     }
 }
