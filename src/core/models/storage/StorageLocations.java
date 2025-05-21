322fb97f757b5ec6093530776fd57e31682493ed
cantilloIsabella <cantilloisabella@uninorte.edu.co>
2025-05-21 11:14:42 -0500

    Load jsonLocations started

--- src/core/models/storage/StorageLocations.java
+++ src/core/models/storage/StorageLocations.java
@@ -5,55 +5,78 @@
 package core.models.storage;
 
 import core.models.Location;
+import core.models.Passenger;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
  *
  * @author Angie
  */
-public class StorageLocations implements Storage<Location>{
-  private static StorageLocations instance;
-  private ArrayList<Location> locations;
-  
-    private StorageLocations(){
-        instance = new StorageLocations();
-        this.locations = new ArrayList<>();
+public class StorageLocations implements Storage<Location> {
+
+    private static StorageLocations instance;
+    private Map<String, Location> locations;
+
+    private StorageLocations() {
+        locations = new HashMap<>();
     }
-    
+
     public static StorageLocations getInstance() {
-        if(instance == null){
+        if (instance == null) {
             instance = new StorageLocations();
         }
         return instance;
     }
 
     @Override
-    public boolean add(Location location) {
-        Location lc = this.get(location.getAirportId());
-         if(lc != null){
-                return false;
+    public boolean add(Object obj) {
+        if (obj instanceof Location) {
+            Location lc = (Location) obj;
+            if (!locations.containsKey(lc.getAirportId())) {
+                locations.put(lc.getAirportId(), lc);
+                return true;
             }
-        locations.add(location);
-        return true;
+        }
+        return false;
     }
 
-    @Override
-    public boolean delete() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
-    public boolean update() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
 
     @Override
     public Location get(String id) {
-       for (Location lc : locations){
-            if(lc.getAirportId().equals(id)){
-                return lc;
-            }
+        try {
+            return locations.get(id);
+        } catch (NotFoundException e) {
+            return null;
         }
-        return null;
+    }
+
+   
+    @Override
+    public boolean delete(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean update(Object obj) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean loadFromJson(String filepath) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+
+    }
+
+    @Override
+    public boolean add(Object obj) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 }
